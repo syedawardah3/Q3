2ceea37f3cb535944e49151843ba811f9e9e9744
syedawardah3 <148571080+syedawardah3@users.noreply.github.com>
2024-02-09 18:36:49 +0500

    Add files via upload

---
+++ Q3/Q3.js
@@ -0,0 +1,8 @@
+// 3.  Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+var personName = "John Doe";
+// Lowercase
+console.log("Lowercase:", personName.toLowerCase());
+// Uppercase
+console.log("Uppercase:", personName.toUpperCase());
+// Titlecase
+console.log("Titlecase:", personName.replace(/\b\w/g, function (char) { return char.toUpperCase(); }));
---
+++ Q3/Q3.ts
@@ -0,0 +1,16 @@
+// 3.  Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+let personName: string = "John Doe";
+
+// Lowercase
+console.log("Lowercase:", personName.toLowerCase());
+
+// Uppercase
+console.log("Uppercase:", personName.toUpperCase());
+
+// Titlecase
+console.log("Titlecase:", personName.replace(/\b\w/g, (char) => char.toUpperCase()));
+
+
+
+
